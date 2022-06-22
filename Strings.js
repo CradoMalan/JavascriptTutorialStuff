796a16436b1c290e334d851655515e8560d4365a
CradoMalan <107924008+CradoMalan@users.noreply.github.com>
2022-06-22 12:15:16 +0100

    commit

---
+++ Strings.js
@@ -0,0 +1,12 @@
+let space = ' '           // an empty space string
+let firstName = 'Asabeneh'
+let lastName = 'Yetayeh'
+let country = 'Finland'
+let city = 'Helsinki'
+let language = 'JavaScript'
+let job = 'teacher'
+let quote = "The saying,'Seeing is Believing' is not correct in 2020."
+let quotWithBackTick = `The saying,'Seeing is Believing' is not correct in 2020.`
+
+let fullName = firstName + space + lastName; // concatenation, merging two string together.
+console.log(fullName);
