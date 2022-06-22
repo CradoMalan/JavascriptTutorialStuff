186b8408e1b8092d74be5c7454371585e6e31fff
CradoMalan <107924008+CradoMalan@users.noreply.github.com>
2022-06-22 12:22:56 +0100

    Updating Strings.js 12:22

--- Day 2/Strings.js
+++ Day 2/Strings.js
@@ -1,4 +1,5 @@
 let space = ' '           // an empty space string
+let age = 100
 let firstName = 'Asabeneh'
 let lastName = 'Yetayeh'
 let country = 'Finland'
@@ -10,3 +11,7 @@ let quotWithBackTick = `The saying,'Seeing is Believing' is not correct in 2020.
 
 let fullName = firstName + space + lastName; // concatenation, merging two string together.
 console.log(fullName);
+
+let personInfoOne = fullName + '. I am ' + age + '. I live in ' + country; // ES5 string addition
+
+console.log(personInfoOne)
